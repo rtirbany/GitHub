3d7e868d821a0212f534157049cb17bce9912d9e
raj <raj@Toshiba2.home>
2012-11-13 01:31:52 -0500

    java and js project setup good basic gui layout - needs work now

--- searchTool/WebContent/app.js
+++ searchTool/WebContent/app.js
@@ -1,7 +1,19 @@
+Ext.Loader.setConfig({
+    enabled: true
+    });
 
-var tpl = Ext.create('Ext.Template',[
-	'Hello {first} {last}',' its nice to meet you!'
-]);
+Ext.application({
+	name : 'MyApp',
+	appFolder : 'app',
+	//requires:['MyApp.view.SearchDemo'],
+	//autoCreateViewport: true,
+	launch : function() {
+		Ext.create('MyApp.view.SearchDemo',{renderTo:Ext.getBody()});
+		Ext.Msg.alert('hi ther');
+		
+	}
+});
+ 
 
 /*var pnlFilter = Ext.create('Ext.form.FormPanel',{
 	itemId:'pnlFilter',
@@ -17,72 +29,28 @@ var tpl = Ext.create('Ext.Template',[
         }]
 });*/
 
-var pnlForm = Ext.create('Ext.form.FormPanel',{
-	itemId:'pnlForm',
-	frame:true,	
-	layout:'anchor',
-	defaultType:'textfield',
-	defaults:{anchor:'-10',labelWidth:65},
-	items:[
-		{fieldLabel:'First Name',name:'first'},
-		{fieldLabel:'Last Name',name:'last'}
-	],
-	buttons:[
-		{text:'Submit',
-		handler:function(){
-			var pnlForm = this.up('#pnlForm');
-			vals = pnlForm.getValues(); 
-			greeting = tpl.apply(vals);
-			Ext.Msg.alert('Hello!',greeting);
-			addChildFilter('divSearchTool','filter_n1','label',vals.first,'#EE00EE');
-			addChildFilter('divSearchTool','filter_n2','label',vals.last);
-		}}
-	]
-}); 
 
-addChildFilter = function(p,n,type,txt,col){
-	var parent = Ext.get(p);
-	//refactor this piece
-	if (col) {
-		var styleColor = 'color:'+col+';';
-		parent.createChild({
-			name:n,
-			tag:type,
-			title:'some description txt',
-			html:txt,
-			style:styleColor,
-			class:'filter'
-		});
-	}
-	else
-	parent.createChild({
-		name:n,
-		tag:type,
-		title:'some description text',
-		html:txt, 
-		class:'filter'
-	});
-	
-};
+
+
 
 //removeAllChildrenFilter = function(p){
 	//var parent = Ext.get(p); 
 //};
 
 
-Ext.onReady(function(){
-	//pnlFilter.show();
-	Ext.create('Ext.window.Window',{
-		height:125,
-		width:200,
-		closable:true,
-		title:'input needed',
-		border: false,
-		layout:'fit',
-		items:[pnlForm]}).show();
-	}
-	
-	);
+//Ext.onReady(function(){
+//	//pnlFilter.show();
+//	Ext.create('Ext.window.Window',{
+//		height:125,
+//		width:200,
+//		closable:true,
+//		title:'input needed',
+//		border: false,
+//		layout:'fit',
+//		items:[pnlForm]}).show();
+//	}
+//	
+//	);
 //TODO: need to remove all, remove by click
 //TODO: ellipsis on overflow (max width set)
 //TODO: how to display if many?  >1 column?
