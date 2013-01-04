5b0709ec6398bbb21c25078a61f1bff726fe220b
Raj <553799@BAHR86M1WX.resource.ds.bah.com>
2013-01-04 10:35:17 -0500

    dyn load sources, adds an 'All' option

--- searchTool/WebContent/app/store/Sources.js
+++ searchTool/WebContent/app/store/Sources.js
@@ -1,15 +1,27 @@
 Ext.define('SearchTool.store.Sources',{
 	extend:'Ext.data.Store', 
 	model:'SearchTool.model.Source',
-	autoLoad:true,
-	//TODO: idProperty set?
-//	idProperty:'SourceId',
+	autoLoad:true, 
+	//TODO: convert to REST 
 	proxy:{
 		type:'ajax',
-		url:'data/sources.json',
+		url:'data/sources.json', 
 		reader:{
 			type:'json',
-			root:'sources'
+			root:'rows',
+			totalProperty:'results',
+			successProperty:'success'
 		}
 	}
+	,
+	listeners:{
+		'load' : 
+			 //TODO: sort this by id..
+			 function(store,records,success, operation){ 
+			 	Ext.Msg.alert(store.getAt(0).getId()+''); 
+				var allChkbox =  Ext.create('SearchTool.model.Source',{'id': 0, 'boxLabel': 'All', 'itemId': 'chkSrcAll', 'name':'chkSrcAll', 'checked':true, 'tooltip':'Srcall tooltip'});
+				store.add(allChkbox);
+			} 
+		 //load listener
+	}//listeners
 });
