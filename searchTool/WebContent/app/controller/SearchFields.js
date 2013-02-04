0871063acc341eca37c03648e8a1e48611f4519b
Raj <553799@BAHR86M1WX.resource.ds.bah.com>
2013-02-04 18:21:37 -0500

    Qbuilder  modified to require data in row in order to build a row

--- searchTool/WebContent/app/controller/SearchFields.js
+++ searchTool/WebContent/app/controller/SearchFields.js
@@ -6,24 +6,30 @@ Ext.define('SearchTool.controller.SearchFields', {
 	requires : ['SearchTool.util.dom'],
 //	stores : ['SearchFields'],
 //	stores : ['QBuilderOperators'],
-	init : function() { 
-		this.control({
+	init : function() {
+		var me = this;
+		me.control({
 					'button[itemId=btnBuildQuery]' : {
-						click : this.buildQuery
+						click : me.buildQuery
 					}
 				});
 	},//init
 	buildQuery : function(b) {
 		var dest = Ext.ComponentQuery.query('#txtSearchBoolean')[0]; 
 		var qbrows = b.up('panel').items.items;
-		var row, s;
+		var row, s, tmpRow='';
+		var last = '';
 		var newval = ''; //to prevent missing entire row data from overwriting prev values 
 		for (var i = 0; i < qbrows.length; i++) {
 			row = qbrows[i];
-			tmp  = '( ' + SearchTool.util.dom.qBuilderRowValidations(i,row)+ ' ) '+ row.down('hidden').getValue() + '\r\n'; 
+			tmpRow = SearchTool.util.dom.qBuilderRowValidations(i,row);
+			last = row.down('hidden').getValue();
+			tmp = (tmpRow == '' ) ? '' : '( '+tmpRow+' )' + last + '\r\n' 
 			newval += tmp;
+			tmpRow = '';
 		}
-		dest.setValue(newval.slice(0,-3));
+		dest.setValue(newval.slice(0,-(last.length+2))); //always remove last 2 chars
+		
 	}
 
 });
