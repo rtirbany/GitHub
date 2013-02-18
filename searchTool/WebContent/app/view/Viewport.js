d856848df2f0486a0e835bceeece1cfe143f0909
Raj <553799@BAHR86M1WX.resource.ds.bah.com>
2013-02-17 21:48:27 -0500

    new quick reference help page beautified viewport, commented out tab, west panels

--- searchTool/WebContent/app/view/Viewport.js
+++ searchTool/WebContent/app/view/Viewport.js
@@ -1,127 +1,121 @@
 Ext.define('SearchTool.view.Viewport', {
-			extend : 'Ext.container.Viewport',
-			itemId : 'main',
-			requires : ['SearchTool.view.main.pnlSearchNav',
-					'SearchTool.view.main.SearchArea',
-					'SearchTool.view.main.ResultsGrid',
-					'SearchTool.view.main.component.WinSave'],
-			layout : 'border', 
-			items : [ 
-					 { //top banner panel 
-						region : 'north',
-						border : false,
-						width : '100%',
-						height : 60,
-						layout : 
-							{	type : 'hbox',
-								defaultMargins : '2px 5px 5px 5px',
-								padding : '2 4 2 4' 
-							}, 
-						items : [
-						{ 
-							//TODO: use 'component' for logo
-							//width:, style:(bordering,padding,etc), html:(img src,style='w:X;h:Y') 
-							xtype : 'tbtext',
-							text : 'CHROME Search'
-						}, {
-							xtype : 'tbfill'
-						},{
-							xtype : 'button',
-							itemId : 'btnHome',
-							text : 'Home',
-							iconCls : 'icon-home'
-						},
-						{
-							xtype : 'button',
-							itemId : 'btnHelp',
-							text : 'Help',
-							iconCls : 'icon-help'
-						}, {
-							xtype : 'button',
-							itemId : 'btnSupport',
-							text : 'Contact Us',
-							iconCls : 'icon-contact'
-						},{
-							xtype : 'tbseparator'
-						}, {
-							xtype : 'tbseparator'
-						}, {
-							xtype : 'button',
-							text : 'Logout',
-							itemId : 'btnLogout',
-							iconCls : 'icon-logout'
-						}
-						]
-					 } // top banner panel
-					 , 
-					{ 		
-						region:'center',
-						xtype : 'tabpanel', 
-						items : [
-							{// searchtab
-							title : 'Search',
-							itemId : 'pnlMainTabSearch',
-							id:'searchMainTabPanel',
-							layout : 'border',
-							items : [
-							{
-								region : 'north',
-								border : false,
-								items : [
-									{
-										xtype:'searchArea',
-										height : 160
-									
-									}
-								]
-							},
-							{
-								region : 'west',
-								itemId : 'pnlSources',
-								title : 'Sources',
-								layout : {
-									type : 'fit',  
-									align : 'stretch'
-								},
-								collapsible : true,
-								animCollapse : true,
-								collapseDirection : 'left',
-								collapsed : true,
-								split : true, 
-								xtype: 'pnlSources',
-								flex : .18
-							},
-							{
-								region : 'west',
-								itemId : 'pnlSearch',
-								title : 'Filters',
-								layout : {
-									type : 'fit',  
-									align : 'stretch'
-								},
-								collapsible : true,
-								animCollapse : true,
-								collapseDirection : 'left',
-								split : true,
-								xtype: 'pnlSearch',
-								flex : .25
-							},
-							
-							{// center
-								region : 'center',
-								border : true,
-							  //  xtype:'resultsgrid',
-							    flex : .75
-							}]
-						}		// searchtab
-						, 
-						{//admin tab
-							title : 'Admin', 
-							html : '(Admin pages go here)',
-							tooltip : 'admin pages appear here'
-						}// admin tab contents
+    extend: 'Ext.container.Viewport',
+    itemId: 'main',
+    requires: ['SearchTool.view.main.pnlSearchNav',
+        'SearchTool.view.main.SearchArea',
+        'SearchTool.view.main.ResultsGrid',
+        'SearchTool.view.main.component.WinSave'],
+    layout: 'border',
+    items: [{ //top banner panel 
+        region: 'north',
+        border: false,
+        width: '100%',
+        height: 60,
+        layout: {
+            type: 'hbox',
+            defaultMargins: '2px 5px 5px 5px',
+            padding: '2 4 2 4'
+        },
+        items: [{
+            //TODO: use 'component' for logo
+            //width:, style:(bordering,padding,etc), html:(img src,style='w:X;h:Y') 
+            xtype: 'tbtext',
+            text: 'CHROME Search'
+        }, {
+            xtype: 'tbfill'
+        }, {
+            xtype: 'button',
+            itemId: 'btnHome',
+            text: 'Home',
+            iconCls: 'icon-home'
+        }, {
+            xtype: 'button',
+            itemId: 'btnHelp',
+            text: 'Help',
+            iconCls: 'icon-help'
+        }, {
+            xtype: 'button',
+            itemId: 'btnSupport',
+            text: 'Contact Us',
+            iconCls: 'icon-contact'
+        }, {
+            xtype: 'tbseparator'
+        }, {
+            xtype: 'tbseparator'
+        }, {
+            xtype: 'button',
+            text: 'Logout',
+            itemId: 'btnLogout',
+            iconCls: 'icon-logout'
+        }]
+    } // top banner panel
+    ,
+    {
+        region: 'center',
+        xtype: 'tabpanel',
+        items: [{ // searchtab
+            title: 'Search',
+            itemId: 'pnlMainTabSearch',
+            id: 'searchMainTabPanel',
+            layout: 'border',
+            items: [{
+                region: 'north',
+                border: false,
+                items: [{
+                    xtype: 'searchArea',
+                    height: 160
 
-					]
-				}]
+                }]
+            }
+            //                            ,
+            //                               {
+            //                                    region : 'west',
+            //                                    itemId : 'pnlSources',
+            //                                    title : 'Sources',
+            //                                    layout : {
+            //                                         type : 'fit',  
+            //                                         align : 'stretch'
+            //                                    },
+            //                                    collapsible : true,
+            //                                    animCollapse : true,
+            //                                    collapseDirection : 'left',
+            //                                    collapsed : true,
+            //                                    split : true, 
+            //                                    xtype: 'pnlSources',
+            //                                    flex : .18
+            //                               },
+            //                               {
+            //                                    region : 'west',
+            //                                    itemId : 'pnlSearch',
+            //                                    title : 'Filters',
+            //                                    layout : {
+            //                                         type : 'fit',  
+            //                                         align : 'stretch'
+            //                                    },
+            //                                    collapsible : true,
+            //                                    animCollapse : true,
+            //                                    collapseDirection : 'left',
+            //                                    split : true,
+            //                                    xtype: 'pnlSearch',
+            //                                    flex : .25
+            //                               }
+            ,
 
-		});
+            { // center
+                region: 'center',
+                border: true,
+                //xtype:'resultsgrid',
+                flex: .75
+            }]
+        } // searchtab
+        //                              , 
+        //                              {//admin tab
+        //                                   title : 'Admin', 
+        //                                   html : '(Admin pages go here)',
+        //                                   tooltip : 'admin pages appear here'
+        //                              }// admin tab contents
+
+        ]
+    }]
+
+});
---
+++ searchTool/WebContent/app/view/help/Reference.js
@@ -0,0 +1,21 @@
+Ext.define('SearchTool.view.help.Reference', {
+    title: 'Quick Reference',
+    resizable: true,
+    floating: true,
+    draggable: true,
+    width: 400,
+    height: 600,
+    frame: true,
+    overflowY: 'auto',
+    overflowX: 'hidden',
+    autoLoad: {
+        url: 'data/acro.html'
+    },
+    tools: [{
+        xtype: 'tool',
+        type: 'close',
+        handler: function (e, target, p, tool) {
+            this.up('panel').close();
+        }
+    }]
+});
