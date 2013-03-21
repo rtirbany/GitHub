07fc45c8d38047fc80cf0f44e7991c692790f6b0
Raj <553799@BAHR86M1WX.resource.ds.bah.com>
2013-03-21 09:46:09 -0400

    cleaned up filtermgmt.js added datachange listener to facetselection store

--- searchTool/WebContent/app/store/FacetSelections.js
+++ searchTool/WebContent/app/store/FacetSelections.js
@@ -3,36 +3,61 @@ Ext.define('SearchTool.store.FacetSelections',{
      requires:['SearchTool.config.Config'],
      model:'SearchTool.model.FacetSelection',
      id:'FacetSelections',
-     data:[
-               {    
-                    'key' : 'size',
-                    'value' : '25 cm',
-                    'tip' : 'hi'
-                    
-               }, {  
-                    'key' : 'weight',
-                    'value' : '2lbs',
-                    'tip' : 'hi'
-               }, { 
-                    'key' : 'mass',
-                    'value' : '4kg',
-                    'tip' : 'hi'
-               }, { 
-                    'key' : 'density',
-                    'value' : '20kg/m2',
-                    'tip' : 'hi'
-               }, { 
-                    'key' : 'viscosity',
-                    'value' : '50%',
-                    'tip' : 'hi'
-               }, { 
-                    'key' : 'thermoconductivity',
-                    'value' : '.25',
-                    'tip' : 'hi'
-               }, { 
-                    'key' : 'element type',
-                    'value' : 'solid metal',
-                    'tip' : 'hi'
-               } 
-     ] 
+     autoLoad:true,
+     proxy:{
+          type:'ajax',
+          url:'data/facetselections.json',
+//        afterRequest:function(req,res){ 
+//        },
+          reader:{ 
+               type:'json',
+               root:'rows',
+               totalProperty:'results',
+               successProperty:'success'
+          }
+     },
+     listeners:{
+          'datachanged' : 
+                //TODO: sort this by id..done? 
+                function(store,records,success, operation){  
+//                  var allChkbox =  Ext.create('SearchTool.model.Source',{'id': 0, 'boxLabel': 'All', 'itemId': 'chkSrcAll', 'name':'chkSrcAll', 'checked':true, 'tooltip':'Srcall tooltip'});
+//                  store.add(allChkbox);
+//                    store.sort('id','ASC');
+                    if (store == 'removed')
+                         Ext.Msg.alert('facet selection changed');
+                    }
+           //load listener
+     }//listeners
+//     data:[
+//               {    
+//                    'key' : 'size',
+//                    'value' : '25 cm',
+//                    'tip' : 'hi'
+//                    
+//               }, {  
+//                    'key' : 'weight',
+//                    'value' : '2lbs',
+//                    'tip' : 'hi'
+//               }, { 
+//                    'key' : 'mass',
+//                    'value' : '4kg',
+//                    'tip' : 'hi'
+//               }, { 
+//                    'key' : 'density',
+//                    'value' : '20kg/m2',
+//                    'tip' : 'hi'
+//               }, { 
+//                    'key' : 'viscosity',
+//                    'value' : '50%',
+//                    'tip' : 'hi'
+//               }, { 
+//                    'key' : 'thermoconductivity',
+//                    'value' : '.25',
+//                    'tip' : 'hi'
+//               }, { 
+//                    'key' : 'element type',
+//                    'value' : 'solid metal',
+//                    'tip' : 'hi'
+//               } 
+//     ] 
 });
--- searchTool/WebContent/app/view/main/component/FilterMgmt.js
+++ searchTool/WebContent/app/view/main/component/FilterMgmt.js
@@ -1,7 +1,7 @@
 var tplData = new Ext.XTemplate(
-     '<tpl for=".">',
-          '<tr><td><div style="width:100%; overflow:hidden;"><input type="button" title="remove this filter" name="remove_{#}" class="facetitemremovebutton"/><label class="facetitem" title="{tip}">{key}&nbsp;=&nbsp;{value}</label></div></td></tr></tpl>'   //use array index to autonumber (starts at 1)
-      
+    '<tpl for=".">',
+    '<tr><td><div style="width:100%; overflow:hidden;"><input type="button" title="remove this filter" name="remove_{#}" class="facetitemremovebutton"/><label class="facetitem" title="{tip}">{key}&nbsp;=&nbsp;{value}</label></div></td></tr></tpl>' //use array index to autonumber (starts at 1)
+
 );
 //
 //var tplDate = new Ext.XTemplate(
@@ -10,101 +10,95 @@ var tplData = new Ext.XTemplate(
 //);
 //
 var mainTpl = new Ext.XTemplate(
-     '<table class="facetgroup">',
-          '<tpl for=".">',
-             '{[this.renderItem(values)]}',
-     '</tpl></table>',
-     {
-          renderItem : function(val) { 
-//               if (val.data.value instanceof Ext.Date) {
-                    var x = tplData.apply(val);
-//                    console.log(x);
-                    return x;
-//               }
-//               else {
-//                  debugger;
-//                  return tplData.apply(val.data);
-//                  }
-          }
-     }
-);
+    '<table class="facetgroup">',
+    '<tpl for=".">',
+    '{[this.renderItem(values)]}',
+    '</tpl></table>', {
+    renderItem: function (val) {
+        //               if (val.data.value instanceof Ext.Date) {
+        var x = tplData.apply(val);
+        //                    console.log(x);
+        return x;
+        //               }
+        //               else {
+        //                  debugger;
+        //                  return tplData.apply(val.data);
+        //                  }
+    }
+});
 
 // tplData.overwrite(filtermgmt.body, dataObjJSON); //pass in root node of data object
 
 Ext.define('SearchTool.view.main.component.FilterMgmt', {
-            extend:'Ext.container.Container',
-            alias:'widget.filtermgmt',
-			border : false,
-            layout:'vbox',
-			items : [
-               {
-                    xtype:'container',
-                    layout:'hbox',
-                    items:[{
-						xtype : 'displayfield',
-						value : 'Filters Options:',
-                        cls: 'filterdisplayfield',
-                        margin: 5,
-						width : 85
-					}, {
-						xtype : 'button',
-//						labelWidth : 65,
-						width : 80,
-                        margin:5,
-                        enableToggle: true,
-						text : 'Remove All'
-					}, {
-						xtype : 'button',
-//						labelWidth : 50,
-						width : 65,
-                         margin:5,
-                        enableToggle: true,
-						text : 'Relax All'
-                    }]//container hbox
-				}
-                    , 
-                {
-			        xtype: 'dataview',
-                    store:'FacetSelections', 
-                    tpl:mainTpl,
-                    autoSync:true,
-                    overflowY: 'auto',
-                    overflowX: 'hidden',
-                    width:'100%',
-                    itemSelector : 'input.facetitemremovebutton',
-                    //overItemCls: 'facetitem-over',
-                    //iconCls: 'icon-btnClear',
-                    emptyText: '(no filters selected)',
-                    listeners:{
-                         itemclick: function(t, r, item, index, e, eOpts){
-                              t.store.data.removeAt(index,1);
-                              //Ext.StoreManager.lookup('FacetSelections').data.removeAt(r.index,1);
-                              //Ext.StoreManager.lookup('FacetSelections').load();
-                              t.refresh();
-                         }
-                    }
-                    }//dataview
-                    
-//                    working panel..too heavy
-//                     xtype: 'grid',
-//                    store:'FacetSelections',
-//                    hideHeaders: true,
-//                    columns : [
-//                       {xtype:'templatecolumn', tpl: '<input type="checkbox" value={key} class="approveCheckbox"/>', width:'40'},
-//                       {xtype:'templatecolumn', tpl: mainTpl, flex:1}
-//                    ],
-//                    overflowY: 'auto',
-//                    overflowX: 'hidden',
-//                    width:'100%'
-                    //tpl: mainTpl
-				//	'<tpl for="tools">', '<div class="appTool">{name}</div>',
-				//	'</tpl>', '</tpl>']
-//                    new Ext.XTemplate('<tpl for=".">', '<div class="filtermgmt"">{group}</div>',
-//                       '<tpl for="tools">', '<div class="appTool">{name}</div>',
-//                        '</tpl>', '</tpl>'),
-                
-                ]//container vbox
+    extend: 'Ext.container.Container',
+    alias: 'widget.filtermgmt',
+    border: false,
+    layout: 'vbox',
+    items: [{
+            xtype: 'container',
+            layout: 'hbox',
+            items: [{
+                    xtype: 'displayfield',
+                    value: 'Filters Options:',
+                    cls: 'filterdisplayfield',
+                    margin: 5,
+                    width: 85
+                }, {
+                    xtype: 'button',
+                    //                            labelWidth : 65,
+                    width: 80,
+                    margin: 5,
+                    text: 'Remove All'
+                }, {
+                    xtype: 'button',
+                    //                            labelWidth : 50,
+                    width: 65,
+                    margin: 5,
+                    enableToggle: true,
+                    text: 'Relax All'
+                }
+            ] //container hbox
+        }, {
+            xtype: 'dataview',
+            store: 'FacetSelections',
+            tpl: mainTpl,
+            autoSync: true,
+            overflowY: 'auto',
+            overflowX: 'hidden',
+            width: '100%',
+            itemSelector: 'input.facetitemremovebutton',
+            //overItemCls: 'facetitem-over',
+            //iconCls: 'icon-btnClear',
+            emptyText: '(no filters selected)',
+            listeners: {
+                itemclick: function (t, r, item, index, e, eOpts) {
+                    t.store.data.removeAt(index, 1);
+                    t.store.fireEvent('datachanged','removed');
+                    t.refresh();
+                }
+            }
+        } //dataview
 
-		});
+        //                    working panel..too heavy
+        //                     xtype: 'grid',
+        //                    store:'FacetSelections',
+        //                    hideHeaders: true,
+        //                    columns : [
+        //                       {xtype:'templatecolumn', tpl: '<input type="checkbox" value={key} class="approveCheckbox"/>', width:'40'},
+        //                       {xtype:'templatecolumn', tpl: mainTpl, flex:1}
+        //                    ],
+        //                    overflowY: 'auto',
+        //                    overflowX: 'hidden',
+        //                    width:'100%'
+        //tpl: mainTpl
+        //     '<tpl for="tools">', '<div class="appTool">{name}</div>',
+        //     '</tpl>', '</tpl>']
+        //                    new Ext.XTemplate('<tpl for=".">', '<div class="filtermgmt"">{group}</div>',
+        //                       '<tpl for="tools">', '<div class="appTool">{name}</div>',
+        //                        '</tpl>', '</tpl>'),
+
+    ] //container vbox
+
+});
 
 // stackoverflow.com/questions/9141918
