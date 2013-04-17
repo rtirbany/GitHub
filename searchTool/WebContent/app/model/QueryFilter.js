8c60608d5e6f200946b3290c0ee6e805cb8c9a6e
Raj <553799@BAHR86M1WX.resource.ds.bah.com>
2013-04-17 13:49:08 -0400

    sync

--- searchTool/WebContent/app/model/QueryFilter.js
+++ searchTool/WebContent/app/model/QueryFilter.js
@@ -1,11 +1,12 @@
 Ext.define('SearchTool.model.QueryFilter',{
      extend:'Ext.data.Model',
-     idProperty : 'value',
+     idProperty: 'id',
      fields:[ 
-              {name:'type',type:'string'},
-              {name:'key',type:'string'},
-              {name:'operator',type:'string'},
-              {name:'value',type:'string'},
-              {name:'tip',type:'string'}
+              {name:'type'},
+              {name:'key'},
+              {name:'id'},
+              {name:'operator'},
+              {name:'value'},
+              {name:'tip'}
             ]
 });
