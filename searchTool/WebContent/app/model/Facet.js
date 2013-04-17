80a3a4ff1b48dbe5ae2fb65afb2e15f3d06967ed
Raj <553799@BAHR86M1WX.resource.ds.bah.com>
2013-04-17 12:16:57 -0400

    sync up

--- searchTool/WebContent/app/model/Facet.js
+++ searchTool/WebContent/app/model/Facet.js
@@ -2,9 +2,24 @@ Ext.define('SearchTool.model.Facet',{
      extend:'Ext.data.Model',
      idProperty : 'value',
      fields:[
-              {name:'value',type:'string'},
-              {name:'operator',type:'string',defaultValue:'='},
-              {name:'count',type:'int'}
+              {name:'value'}, 
+              {name:'operator',type:'string',defaultValue:'eq'},
+              {name:'count',type:'int',convert:null}
             ],
      belongsTo:'SearchTool.model.FacetGroup'
 });
+
+//Ext.define('SearchTool.model.Facet',{
+//     extend:'Ext.data.Model',
+//     idProperty : 'value',
+//     fields:[
+//              'value',
+//              'count'
+//            ],
+//     associations:[{
+//          type: 'belongsTo',
+//          model:'SearchTool.model.FacetGroup',
+//          name:'facetName',
+//          associationKey: 'FACETNAME'
+//     }]
+//});
--- searchTool/WebContent/app/model/FacetGroup.js
+++ searchTool/WebContent/app/model/FacetGroup.js
@@ -2,8 +2,24 @@ Ext.define('SearchTool.model.FacetGroup',{
      extend:'Ext.data.Model',
      idProperty : 'facetName',
      fields:[
-              {name:'facetName',type:'string'},
+              {name:'facetName'},
               {name:'facetEntries', type:'auto'}
               ],
      hasMany: [{model: 'SearchTool.model.Facet',name:'facetEntries',associationKey:'facetEntries'}]
 });
+
+//Ext.define('SearchTool.model.FacetGroup',{
+//     extend:'Ext.data.Model',
+//     idProperty : 'facetName',
+//     fields:[
+//              'facetName'
+////              ,{name:'facetEntries', type:'auto'}
+//              ],
+//     associations: [
+//          {type: 'hasMany',
+//          model : 'Facet',
+//          name:'facetEntries',
+//          associationKey: 'FACETENTRIES'
+//          }
+//          ]
+//});
