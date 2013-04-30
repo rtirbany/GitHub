6c7d89bba0e221ae8e23d7279892df4595c395fc
Raj <553799@BAHR86M1WX.resource.ds.bah.com>
2013-04-29 23:43:03 -0400

    not sync

--- searchTool/WebContent/app/model/Result.js
+++ searchTool/WebContent/app/model/Result.js
@@ -2,13 +2,13 @@ Ext.define('SearchTool.model.Result',{
 	extend:'Ext.data.Model',
 	idProperty : 'id',
 	fields:[
-			{name:'id',type:'int'},
+			{name:'id',type:'int',convert:null},
 			{name:'dt',type:'date'}, //dateFormat: 'm-d-Y g:i A'
-		    {name:'source',type:'string'},
-		    {name:'product',type:'string'},
-		    {name:'summary',type:'string'},
-		    {name:'serial', type:'string'},
-		    {name:'subject',type:'string'}
+		    {name:'source'},
+		    {name:'product'},
+		    {name:'summary'},
+		    {name:'serial'},
+		    {name:'subject'}
 	]
 });
 	     
