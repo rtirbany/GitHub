0c876e1e73fb480875525cda72546e1f6150b1f5
Raj <553799@BAHR86M1WX.resource.ds.bah.com>
2013-05-29 02:51:38 -0400

    timeout functionality

--- searchTool/WebContent/app.js
+++ searchTool/WebContent/app.js
@@ -2,45 +2,86 @@ Ext.Loader.setConfig({
     enabled: true
     });
 
-Ext.onReady(function() { 
-	Ext.getBody().mask('Loading Application.  Please wait...'); 
-	/*
-	// Start the mask on the body and get a reference to the mask
-    // add class to mask for it to look different than default.
-    splashscreen = Ext.getBody().mask('Loading application.  Please wait...', 'splashscreen');
-    splashscreen.addCls('splashscreen');
-
-    // Insert a new div before the loading icon where we can place our logo.
-    Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
-        cls: 'x-splash-icon'
-    });*/
-});
+function CheckOWF(){
+     return false; //Ozone.util.isRunningInOWF();
+}
 
 function LaunchViz(){
-     Ext.Msg.alert('launching viz');
+     if (CheckOWF()){
+          Ozone.pref.PrefServer.findWidgets(
+          {searchParams:{widgetName: SearchTool.config.Config.vizWidgetName},
+               onSuccess:function(w){
+                    if (w.length > 0){
+                         var vizParams = {searchId: btn.id};
+                         OWF.Launcher.launch({
+                              guid: w[0].id,
+                              launchOnlyIfClosed: SearchTool.config.Config.vizWidgetSingleton,
+                              data: OWF.Util.toString(vizParams)
+                         });
+                    }
+                    else {
+                         Ext.Msg.alert('Error locating widget ('+ SearchTool.config.Config.vizWidgetName+' )',SearchTool.config.Config.msgErrorContactAdmin);
+                    }
+               },
+               onFailure: function(w,e){
+                    Ext.Msg.alert('Error communicating with OWF Server',SearchTool.config.Config.msgErrorTryAgain);
+               }
+          }
+          ); 
+     } 
      // if Ozone.util.isRunningInOWF(){}...
 }
+
+
+Ext.onReady(function() { 
+	Ext.getBody().mask('Loading Application.  Please wait...');
+	// Start the mask on the body and get a reference to the mask
+    // add class to mask for it to look different than default.
+//    splashscreen = Ext.getBody().mask('Loading application.  Please wait...', 'splashscreen');
+//    splashscreen.addCls('splashscreen');
+
+    // Insert a new div before the loading icon where we can place our logo.
+//    Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
+//        cls: 'x-splash-icon'
+//    }); 
+});
+
 Ext.application({
 	name : 'SearchTool',
 	paths : {'Ext.ux':'extjs/ux'},
 	appFolder : 'app',
 	controllers:['SearchTool','SearchFields','CustomDateRange','QueryFilters'],
-	requires:['SearchTool.config.Config','SearchTool.util.TplFilter'],
+	requires:['SearchTool.config.Config','SearchTool.util.TplFilter','SearchTool.util.SessionMonitor'],
 	autoCreateViewport: true,
+     namnam: function(){
+          Ext.Msg.alert('hi');
+     },
 	launch : function() {  
     	Ext.tip.QuickTipManager.init();
+        //Ext.EventManager.on(window,'beforeunload',function(){ 
+        //})
+        
         //Ext.Ajax.timeout= SearchTool.config.Config.ajaxTimout; 
         //Ext.override(Ext.form.Basic, {timeout:Ext.Ajax.timeout/1000});
         //Ext.override(Ext.data.proxy.Server,{timeout:Ext.Ajax.timeout});
         //Ext.override(Ext.data.Connection,{timeout:Ext.Ajax.timeout});
-    	if(typeof String.prototype.trim !== 'function') {
+    	if (typeof String.prototype.trim !== 'function') {
   					String.prototype.trim = function() {
     					return this.replace(/^\s+|\s+$/g, ''); 
   					}
-				}
+	    }
+        /*
+         * if ( !Array.prototype.forEach ) 
+         * { Array.prototype.forEach = function(fn, scope) 
+         * { for(var i = 0, len = this.length; i < len; ++i) { fn.call(scope, this[i], i, this); } } }
+         * 
+         * */
+        if (CheckOWF())
+             Ext.ComponentQuery.query('#btnVisualize')[0].hide();
 		Ext.getBody().unmask();
-		/* RT - if using full screen mask w/ logo, add this to fade out mask + logo
-		 * var task = new Ext.util.DelayedTask(function() {
+        SearchTool.util.SessionMonitor.start();
+		// RT - if using full screen mask w/ logo, add this to fade out mask + logo
+		/*   var task = new Ext.util.DelayedTask(function() {
             // Fade out the body mask
             splashscreen.fadeOut({
                 duration: 1000,
@@ -59,9 +100,9 @@ Ext.application({
             });
         });
         // Run the fade 500 milliseconds after launch.
-        task.delay(500);
-		 * */
-	}
+        task.delay(500); 
+	}*/
+     }
 });
  
 
