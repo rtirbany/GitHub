05a6f1de2fea50e5133c9d5194b82f3c902fa92b
Raj <553799@BAHR86M1WX.resource.ds.bah.com>
2013-06-03 20:12:28 -0400

    session timeout fix

--- searchTool/WebContent/app/util/SessionMonitor.js
+++ searchTool/WebContent/app/util/SessionMonitor.js
@@ -1,26 +1,22 @@
 Ext.define('SearchTool.util.SessionMonitor', {
+  requires:['SearchTool.config.Config'],
   singleton: true,
   interval: 1000 * 10,  // run every 10 seconds.
   lastActive: null,
-  maxInactive: 1000 * 60 * 15,  // 15 minutes of inactivity allowed; set it to 1 for testing.
   remaining: 0,
-  ui: Ext.getBody(),
-
-  /**
-   * Dialog to display expiration message and count-down timer.
-   */
+  ui: Ext.getBody(), 
   window: Ext.create('Ext.window.Window', {
-    bodyPadding: 5,
+    title: 'Session Timeout',
     closable: false,
-    closeAction: 'hide',
+    bodyPadding: 5,
     modal: true,
+    closeAction: 'hide',
     resizable: false,
-    title: 'Session Timeout Warning',
     width: 325,
     items: [{
       xtype: 'container',
       frame: true,
-      html: "Your session will automatically expires after 15 minutes of  inactivity. If your session expires, any unsaved data will be lost and you will be automatically logged out. </br></br>If you want to continue working, click 'Continue'.</br></br>"    
+      html: "Your session is about to timeout due to inactivity. Any unsaved data will be lost and you will be automatically logged out if this occurs. </br></br>If you wish to extend your session and continue working, click the 'Continue' button below.</br></br>"    
     },{
       xtype: 'label',
       text: ''
@@ -48,15 +44,9 @@ Ext.define('SearchTool.util.SessionMonitor', {
       }
     }]
   }),
-
- 
-  /**
-   * Sets up a timer task to monitor for mousemove/keydown events and
-   * a count-down timer task to be used by the 60 second count-down dialog.
-   */
   constructor: function(config) {
     var me = this;
-   
+    me.maxInactive = 1000 * 60 * SearchTool.config.Config.sessionTimeout,  // 15 minutes of inactivity allowed; set it to 1 for testing.
     // session monitor task
     this.sessionTask = {
       run: me.monitorUI,
@@ -71,20 +61,10 @@ Ext.define('SearchTool.util.SessionMonitor', {
       interval: 1000,
       scope: me
     };
-  },
- 
- 
-  /**
-   * Simple method to register with the mousemove and keydown events.
-   */
+  }, 
   captureActivity : function(eventObj, el, eventOptions) {
     this.lastActive = new Date();
-  },
-
-
-  /**
-   *  Monitors the UI to determine if you've exceeded the inactivity threshold.
-   */
+  }, 
   monitorUI : function() {
     var now = new Date();
     var inactive = (now - this.lastActive);
@@ -93,43 +73,36 @@ Ext.define('SearchTool.util.SessionMonitor', {
       this.stop();
 
       this.window.show();
-      this.remaining = 60;  // seconds remaining.
+      this.remaining = SearchTool.config.Config.sessoinGracePeriod;  // seconds remaining.
       Ext.TaskManager.start(this.countDownTask);
     }
   },
-
- 
-  /**
-   * Starts the session timer task and registers mouse/keyboard activity event monitors.
-   */
   start : function() { 
     this.lastActive = new Date();
 
     this.ui.on('mousemove', this.captureActivity, this);
     this.ui.on('keydown', this.captureActivity, this);
+    this.ui.on('mousedown', this.captureActivity, this);
         
     Ext.TaskManager.start(this.sessionTask);
-  },
- 
- 
+  }, 
   stop: function() {
     Ext.TaskManager.stop(this.sessionTask);
     this.ui.un('mousemove', this.captureActivity, this);  //  always wipe-up after yourself...
     this.ui.un('keydown', this.captureActivity, this);
-  },
- 
- 
-  /**
-   * Countdown function updates the message label in the user dialog which displays
-   * the seconds remaining prior to session expiration.  If the counter expires, you're logged out.
-   */
+    this.ui.un('mousedown', this.captureActivity, this);
+  }, 
   countDown: function() {
     this.window.down('label').update('Your session will expire in ' +  this.remaining + ' second' + ((this.remaining == 1) ? '.' : 's.') );
     
     --this.remaining;
 
-    if (this.remaining < 0) {
-      this.window.down('button[action="logout"]').handler();
+    if (this.remaining < 0) { 
+      var m = Ext.ComponentQuery.query('#main')[0];
+      this.stop();
+      this.window.hide(); //TODO: get the Ext Window Manager and removeAll()
+      m.removeAll(false);
+      
     }
   }
  
